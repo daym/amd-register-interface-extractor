2891c71492415a35bb4c43358ec38fd1da74c6ad
Danny Milosavljevic <dannym@scratchpost.org>
2021-02-22 12:14:45 +0100

    compiler: Make pdf2html less spammy.

--- compiler.mk
+++ compiler.mk
@@ -2,23 +2,23 @@
 
 partsvol1/a.xml: $(PPRVOL1)
 	mkdir -p partsvol1
-	pdftohtml -xml $< partsvol1/a
+	pdftohtml -xml $< partsvol1/a >/dev/null
 
 partsvol2/a.xml: $(PPRVOL2)
 	mkdir -p partsvol2
-	pdftohtml -xml $< partsvol2/a
+	pdftohtml -xml $< partsvol2/a >/dev/null
 
 partsvol3/a.xml: $(PPRVOL3)
 	mkdir -p partsvol3
-	pdftohtml -xml $< partsvol3/a
+	pdftohtml -xml $< partsvol3/a >/dev/null
 
 partsvol4/a.xml: $(PPRVOL4)
 	mkdir -p partsvol4
-	pdftohtml -xml $< partsvol4/a
+	pdftohtml -xml $< partsvol4/a >/dev/null
 
 partsvol5/a.xml: $(PPRVOL5)
 	mkdir -p partsvol5
-	pdftohtml -xml $< partsvol5/a
+	pdftohtml -xml $< partsvol5/a >/dev/null
 
 resultvol1.txt: partsvol1/a.xml extract.py
 	./extract.py $< > "$@".new && mv "$@".new "$@"
