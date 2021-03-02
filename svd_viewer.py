433498dd48fcb131f97a9cecfa7226678477aa01
Danny Milosavljevic <danny.milo@datacom.wien>
2021-03-02 10:51:39 +0100

    svd_viewer: Move description to be the last field.

--- svd_viewer.py
+++ svd_viewer.py
@@ -14,11 +14,11 @@ model = Gtk.TreeStore(str, str, str) # type, name, tooltip
 
 # Note: optional: device: vendor, vendorID, series, licenseText, access.
 type_to_pseudo_attributes = {
-	"device": ["name", "version", "description", "addressUnitBits", "width", "size", "resetValue", "resetMask", "vendor", "vendorID", "series", "licenseText", "access"],
-	"peripheral": ["name", "description", "groupName", "baseAddress", "addressBlock", "resetValue", "resetMask"],
-	"register": ["name", "displayName", "description", "addressOffset", "size", "access", "resetValue", "resetMask", "alternateRegister"],
-	"field": ["name", "description", "bitOffset", "bitWidth", "access", "bitRange", "msb", "lsb"], # note: bitRange, msb and lsb are optional.
-	"interrupt": ["name", "description", "value"],
+	"device": ["name", "version", "addressUnitBits", "width", "size", "resetValue", "resetMask", "vendor", "vendorID", "series", "licenseText", "access", "description"],
+	"peripheral": ["name", "groupName", "baseAddress", "addressBlock", "resetValue", "resetMask", "description"],
+	"register": ["name", "displayName", "addressOffset", "size", "access", "resetValue", "resetMask", "alternateRegister", "description"],
+	"field": ["name", "bitOffset", "bitWidth", "access", "bitRange", "msb", "lsb", "description"], # note: bitRange, msb and lsb are optional.
+	"interrupt": ["name", "value", "description"],
 }
 
 scroller = Gtk.ScrolledWindow()
