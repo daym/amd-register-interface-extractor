d948fecd75dacc15a3943af8c5a4addafa097bcb
Danny Milosavljevic <danny.milo@datacom.wien>
2021-03-02 10:27:20 +0100

    svd_viewer: Add resetMask to register view.

--- svd_viewer.py
+++ svd_viewer.py
@@ -16,7 +16,7 @@ model = Gtk.TreeStore(str, str, str) # type, name, tooltip
 type_to_pseudo_attributes = {
 	"device": ["name", "version", "description", "addressUnitBits", "width", "size", "resetValue", "resetMask", "vendor", "vendorID", "series", "licenseText", "access"],
 	"peripheral": ["name", "description", "groupName", "baseAddress", "addressBlock"],
-	"register": ["name", "displayName", "description", "addressOffset", "size", "access", "resetValue", "alternateRegister"],
+	"register": ["name", "displayName", "description", "addressOffset", "size", "access", "resetValue", "resetMask", "alternateRegister"],
 	"field": ["name", "description", "bitOffset", "bitWidth", "access", "bitRange", "msb", "lsb"], # note: bitRange, msb and lsb are optional.
 	"interrupt": ["name", "description", "value"],
 }
