5de94336f274763f92388f5009440fba4e99dc14
Danny Milosavljevic <danny.milo@datacom.wien>
2021-03-04 12:27:05 +0100

    configure: Support Genoa.

--- Makefile
+++ Makefile
@@ -6,11 +6,11 @@ all: phase3_host.svd phase3_io.svd phase3_msr.svd phase3_smn.svd
 include config.mk
 include compiler.mk
 
-result.txt: $(if $(PPRVOL1),resultvol1.txt,) $(if $(PPRVOL2),resultvol2.txt,) $(if $(PPRVOL3),resultvol3.txt,)
+result.txt: $(if $(PPRVOL1),resultvol1.txt,) $(if $(PPRVOL2),resultvol2.txt,) $(if $(PPRVOL3),resultvol3.txt,) $(if $(PPRVOL4),resultvol4.txt,) $(if $(PPRVOL5),resultvol5.txt,) $(if $(PPRVOL6),resultvol6.txt,) $(if $(PPRVOL7),resultvol7.txt,) $(if $(PPRVOL8),resultvol8.txt,) $(if $(PPRVOL9),resultvol9.txt,)
 	cat $^ > "$@.new" && mv "$@.new" "$@"
 
 clean:
-	rm -rf result.txt resultvol1.txt resultvol2.txt resultvol3.txt resultvol4.txt resultvol5.txt phase2_result.py phase3_*.svd partsvol1 partsvol2 partsvol3 partsvol4 partsvol5
+	rm -rf result.txt resultvol1.txt resultvol2.txt resultvol3.txt resultvol4.txt resultvol5.txt resultvol6.txt resultvol7.txt resultvol8.txt resultvol9.txt phase2_result.py phase3_*.svd partsvol1 partsvol2 partsvol3 partsvol4 partsvol5 partsvol6 partsvol7 partsvol8 partsvol9
 
 distclean: clean
 
--- compiler.mk
+++ compiler.mk
@@ -20,6 +20,22 @@ partsvol5/a.xml: $(PPRVOL5)
 	mkdir -p partsvol5
 	pdftohtml -xml $< partsvol5/a >/dev/null
 
+partsvol6/a.xml: $(PPRVOL6)
+	mkdir -p partsvol6
+	pdftohtml -xml $< partsvol6/a >/dev/null
+
+partsvol7/a.xml: $(PPRVOL7)
+	mkdir -p partsvol7
+	pdftohtml -xml $< partsvol7/a >/dev/null
+
+partsvol8/a.xml: $(PPRVOL8)
+	mkdir -p partsvol8
+	pdftohtml -xml $< partsvol8/a >/dev/null
+
+partsvol9/a.xml: $(PPRVOL9)
+	mkdir -p partsvol9
+	pdftohtml -xml $< partsvol9/a >/dev/null
+
 resultvol1.txt: partsvol1/a.xml extract.py
 	./extract.py $< > "$@".new && mv "$@".new "$@"
 
@@ -33,6 +49,18 @@ resultvol4.txt: partsvol4/a.xml extract.py
 	./extract.py $< > "$@".new && mv "$@".new "$@"
 
 resultvol5.txt: partsvol5/a.xml extract.py
+	./extract.py $< > "$@".new && mv "$@".new "$@"
+
+resultvol6.txt: partsvol6/a.xml extract.py
+	./extract.py $< > "$@".new && mv "$@".new "$@"
+
+resultvol7.txt: partsvol7/a.xml extract.py
+	./extract.py $< > "$@".new && mv "$@".new "$@"
+
+resultvol8.txt: partsvol8/a.xml extract.py
+	./extract.py $< > "$@".new && mv "$@".new "$@"
+
+resultvol9.txt: partsvol9/a.xml extract.py
 	./extract.py $< > "$@".new && mv "$@".new "$@"
 
 phase2_result.py: result.txt phase2.py
